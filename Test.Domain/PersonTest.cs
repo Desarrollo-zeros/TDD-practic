4e3e8e63756158030660ded1f13ba2f6d9d8a6a7
Juan David <jdcastillag147@gmail.com>
2023-10-21 10:45:33 -0500

    create test

--- Test.Domain/PersonTest.cs
+++ Test.Domain/PersonTest.cs
@@ -1,12 +1,85 @@
-﻿using System;
+﻿using Domain.Contract;
+using Domain.Entity;
+using Domain.Services;
+using NUnit.Framework;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection.Metadata;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Test.Domain
 {
-    internal class PersonTest
+    [TestFixture]
+    public class PersonTest
     {
+        private IPersonService _personService;
+
+        [SetUp]
+        public void SetUp()
+        {
+
+        }
+
+        [Test]
+        public void DeleteSuccesWithData()
+        {
+            var people = new List<Person>
+            {
+                new Person()
+                {
+                    Id = 1,
+                    CreatedAt = DateTime.Now,
+                    FirstName = "Juan",
+                    SecondName = "David",
+                    LastName = "Castilla",
+                    SecondLastName = "García",
+                    Document = "1066874827",
+                    UserId = 1,
+                    User = new User()
+                }
+            };
+
+            _personService = new PersonService(people);
+            var result = _personService.Delete(1);
+            var resultResult = _personService.Get(1);
+            Assert.IsNotNull(result);
+            Assert.IsTrue(result);
+            Assert.IsNull(resultResult);
+        }
+
+        [Test]
+        public void DeleteFailWithNotData()
+        {
+            _personService = new PersonService();
+            var result = _personService.Delete(2);
+            Assert.IsNotNull(result);
+            Assert.IsFalse(result);
+        }
+
+        [Test]
+        public void DeleteFailWithData()
+        {
+            var people = new List<Person>
+            {
+                new Person()
+                {
+                    Id = 1,
+                    CreatedAt = DateTime.Now,
+                    FirstName = "Juan",
+                    SecondName = "David",
+                    LastName = "Castilla",
+                    SecondLastName = "García",
+                    Document = "1066874827",
+                    UserId = 1,
+                    User = new User()
+                }
+            };
+            _personService = new PersonService(people);
+            var result = _personService.Delete(2);
+            Assert.IsNotNull(result);
+            Assert.IsFalse(result);
+        }
     }
 }
