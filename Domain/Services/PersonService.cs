db5bde2080280d63462a2a95b665f33a0b1ec287
david_agamez <david.agamez@school.devzeros.com>
2023-10-21 09:40:50 -0500

    AddGetAll

--- Domain/Services/PersonService.cs
+++ Domain/Services/PersonService.cs
@@ -27,6 +27,53 @@ namespace Domain.Services
 
         public List<C> GetAll<C>(string key = "", string opera = "=", string value = "") where C : class
         {
+            if (Person.count == 0)
+                throw new Exception("No hay datos para consultar");
+            var list = new List<C>();
+
+            if (typeof(C).Name == "User")
+            {
+                foreach (var user in Users)
+                {
+                    if (user is C userC)
+                    {
+                        list.Add(userC);
+                    }
+                }
+                return list;
+            }
+            else
+            {
+                if (!string.IsNullOrWhiteSpace(key)
+                         && string.IsNullOrWhiteSpace(opera) &&
+                         string.IsNullOrWhiteSpace(value))
+                {
+
+                    if (typeof(User).GetProperty(key) == null)
+                    {
+                        throw new Exception("La propieda a consultar no existe");
+                    }
+                    var keyValues = Users.Select(user =>
+                    {
+                        var property = user.GetType().GetProperty(key);
+                        if (property != null)
+                        {
+                            return property.GetValue(user);
+                        }
+                        return null; // O puedes devolver un valor predeterminado en caso de que la propiedad no exista
+                    }).ToList();
+
+                    foreach (var keys in keyValues)
+                    {
+                        if (keys is C keyC)
+                        {
+                            list.Add(keyC);
+                        }
+                    }
+                }
+            }
+            return list;
+        
             throw new NotImplementedException();
         }
 
